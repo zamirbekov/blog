d10c3337e29d0f715163ef997156f2f515b491a3
Zamirbekov <saku.kairat@gmail.com>
2018-03-17 19:29:15 +0600

    Create a new project blog, new apps authors, articles. Add debug-toolbar.

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.conf import settings
+from django.urls import include, path, re_path
+from articles import views as articles_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', articles_views.index, name='index'),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
